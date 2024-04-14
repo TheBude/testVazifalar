9e7fbd2c9d1e5946dc15293b2b7b19c3464f5edb
TheBude <husaenbobmuratov192@gmail.com>
2024-04-14 18:35:38 +0500

    massivlar

--- testVazifalar/Program.cs
+++ testVazifalar/Program.cs
@@ -1,11 +1,18 @@
-﻿//toq sonlar yig'indisi
-int num = 0;
-int i = 1;
+﻿int[] myArray  = {15, 60, 10, 23, 20};
 
-while (i < 1000)
+int minVal = myArray[0];
+int maxVal = myArray[0];
+
+for (int i = 0; i < myArray.Length; i++)
 {
-    num += i;
-    i += 2;
+    if (myArray[i] < minVal)
+    {
+        minVal = myArray[i];
+    }
+    if (myArray[i] > maxVal)
+    {
+        maxVal = myArray[i];
+    }
 }
 
-Console.WriteLine("Toqsonlar yig'indisi:  " + num);
+Console.WriteLine($"Eng kichik qiymati: {minVal}\nEng aktta qiymati: {maxVal}\nIkki qiymatni ko'paytmasi: {minVal * maxVal}");
